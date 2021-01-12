e67f6b7f476036b544dc5f69f9b0203cdc781635
inqueblot <notminebydesign@gmail.com>
2021-01-12 17:05:13 -0600

    working JS, app functional and ready for release

---
+++ Assets/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    var now = moment();
+    var hour = now._d.getHours();
+
+    $("#currentDay").text(now.format("dddd, MMMM Do"))
+    // for each time block, set text from local storage and colors based on time
+    $("textarea").each(function () {
+        var savedData = localStorage.getItem($(this).attr("value"));
+        $(this).text(savedData);
+
+        var timeTag = parseInt($(this).attr('value'));
+        console.log(timeTag)
+        console.log(hour)
+
+        if (timeTag < hour) {
+            $(this).addClass("past")
+        }
+        if (timeTag > hour) {
+            $(this).addClass("future")
+        }
+        if (timeTag == hour) {
+            $(this).addClass("present")
+        };
+
+    });
+
+    $(".saveBtn").on("click", function () {
+        localStorage.setItem($(this).prev().attr("value"), $(this).prev().val())
+    });
+
+});
